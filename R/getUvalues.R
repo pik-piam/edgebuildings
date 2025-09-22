81ddcc187815706a801ca7885afd1638d5e9aa84
Ricarda Rosemann <ricarda.rosemann@pik-potsdam.de>
2025-09-22 11:53:44 +0200

    Revert "added climate elasticity and improved non-increasement of u-values"
    This reverts commit 25eac5ff89e8ae0b2e927398549c90a9f169aca8.

--- R/getUvalues.R
+++ R/getUvalues.R
@@ -68,23 +68,16 @@ getUValues <- function(config,
     removeColNa()
 
 
-  # scenario-specific temporal convergence assumptions
+  # scenario convergence speed assumptions
   scenAssumpSpeed <- scenAssumpSpeed %>%
     filter(.data[["scenario"]] == scen)
 
 
-  # scenario assumptions on percentage of future u-values rel. to hist. projections
+  # minimal U-value
   scenAssumpFactor <- config[scen, "econCoef_uvalues_X_min"] %>%
     buildScenInput(subtype = "mapping",
                    regionmap = regionalmap) %>%
     rename(scenAssumpFactor = "value")
-
-
-  # elasticity of marginal influence of climate factors beyond endOfHistory
-  climateElas <- config[scen, "uvalues_climateElas"] %>%
-    buildScenInput(subtype = "mapping",
-                   regionmap = regionalmap) %>%
-    rename("climateElas" = "value")
 
 
   # calculate temporal convergence shares
@@ -107,66 +100,28 @@ getUValues <- function(config,
     semi_join(lambda, by = "period")
 
 
-  # obtain 5 year mean of last historical values to reduce bias of single data point
-  dataEOH <- data %>%
-    filter(.data$period %in% seq.int(endOfHistory - 4, endOfHistory)) %>%
-    group_by(across(all_of(c("region", "scenario")))) %>%
-    reframe(eohHDD    = mean(.data$HDD,    na.rm = TRUE),
-            eohCDD    = mean(.data$CDD,    na.rm = TRUE),
-            eohGDPPOP = mean(.data$gdppop, na.rm = TRUE))
-
-
+  # project historical and future u-values
   data <- data %>%
-    # project historical and future u-values
-    left_join(dataEOH, by = c("region", "scenario")) %>%
-    left_join(climateElas, by = "region") %>%
-    mutate(uvalueProj = ifelse(.data$period <= endOfHistory,
+    mutate(uvalueProj = exp(.data$parINTERCEPT +
+                              .data$parHDD * .data$HDD +
+                              .data$parCDD * .data$CDD +
+                              .data$parGDPPOP * log(.data$gdppop + 1) +
+                              .data$parHDDGDPPOP * .data$HDD * log(.data$gdppop + 1) +
+                              .data$parCDDGDPPOP * .data$CDD * log(.data$gdppop + 1)) +
+             .data$minU) %>%
 
-                               # Historical periods: use original regression formula
-                               exp(.data$parINTERCEPT +
-                                     .data$parHDD * .data$HDD +
-                                     .data$parCDD * .data$CDD +
-                                     .data$parGDPPOP * log(.data$gdppop + 1) +
-                                     .data$parHDDGDPPOP * .data$HDD * log(.data$gdppop + 1) +
-                                     .data$parCDDGDPPOP * .data$CDD * log(.data$gdppop + 1)) +
-                                 .data$minU,
-
-                               # Future periods: apply climate dampening
-                               exp(.data$parINTERCEPT +
-                                     .data$parHDD * (.data$eohHDD * (.data$HDD / .data$eohHDD)^.data$climateElas) +
-                                     .data$parCDD * (.data$eohCDD * (.data$CDD / .data$eohCDD)^.data$climateElas) +
-                                     .data$parGDPPOP * log(.data$gdppop + 1) +
-                                     .data$parHDDGDPPOP * (.data$eohHDD * (.data$HDD / .data$eohHDD)^.data$climateElas)
-                                     * log(.data$gdppop + 1) +
-                                     .data$parCDDGDPPOP * (.data$eohCDD * (.data$CDD / .data$eohCDD)^.data$climateElas)
-                                     * log(.data$gdppop + 1)) +
-                                 .data$minU)) %>%
-
-    # include scenario assumptions
+    # include scenario assumptionsx
     left_join(scenAssumpFactor, by = "region") %>%
     left_join(lambda, by = c("region", "period", "scenario")) %>%
 
     # merge historical and scenario projections
     mutate(uvalueScen = .data$uvalueProj * .data$scenAssumpFactor,
-           uvalue     = .data$uvalueProj * (1 - .data$fullconv) + .data$uvalueScen * .data$fullconv) %>%
+           uvalue = .data$uvalueProj * (1 - .data$fullconv) + .data$uvalueScen * .data$fullconv) %>%
 
     # smooth historical u-values and prohibit decrease over time
     group_by(across(all_of(c("region", "scenario")))) %>%
-    mutate(uvalueSmooth  = ifelse(.data$period >= endOfHistory, .data$uvalue,  lowpass(.data$uvalue, i = 5))) %>%
-    filter(!is.na(.data$uvalueSmooth)) %>%
-
-    # Find max historical value and period where it occurs
-    mutate(maxHistValue = max(.data$uvalueSmooth[.data$period <= endOfHistory], na.rm = TRUE),
-           periodMax    = .data$period[.data$period <= endOfHistory]
-           [which.max(.data$uvalueSmooth[.data$period <= endOfHistory])]) %>%
-
-    # enusure that u-values do not increase over time:
-    #   1) periods before historical max: apply max value
-    #   2) periods at max and after: keep original values
-    #   3) apply cumulative min to full time-series
-    arrange(.data$period) %>%
-    mutate(uvalueCorrected = ifelse(.data$period < .data$periodMax, .data$maxHistValue, .data$uvalueSmooth),
-           uvalueMin       = cummin(.data$uvalueCorrected)) %>%
+    mutate(uvalueSmooth = ifelse(.data$period >= endOfHistory, .data$uvalue, lowpass(.data$uvalue, i = 5)),
+           uvalueMin = cummin(.data$uvalueSmooth)) %>%
     ungroup() %>%
 
     select("region", "period", "scenario", "uvalue" = "uvalueMin") %>%
